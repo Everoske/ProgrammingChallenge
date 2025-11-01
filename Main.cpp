d23685a984f143f83e6786cf71005152a4987556
Ellevern <elisavandk@gmail.com>
2025-11-01 00:04:09 -0400

    Added functions for testing different solution approaches.

--- Main.cpp
+++ Main.cpp
@@ -10,37 +10,45 @@
 #include "EVector3.h"
 
 
+void algorithmSortWithSquareMag(int tPoints);
+void algorithmSortWithMag(int tPoints);
+void binaryTreeWithSquareMag(int tPoints);
+void binaryTreeWithMag(int tPoints);
+
 int main()
 {
 	const auto start{ std::chrono::steady_clock::now() };
 	std::srand(std::time({}));
-	const int totalPoints = std::rand() % 1001;
+	//const int totalPoints = std::rand() % 1001;
+	const int totalPoints = 1000;
 	std::cout << "Total Points: " << totalPoints << std::endl;
+	//algorithmSortWithSquareMag(totalPoints);
+	//algorithmSortWithMag(totalPoints);
 
+	const auto finish{ std::chrono::steady_clock::now() };
+	const std::chrono::duration<double> elapsedSeconds{ finish - start };
+	std::cout << "Elapsed Time: " << elapsedSeconds.count() << std::endl;
+
+	return 0;
+}
+
+void algorithmSortWithSquareMag(int tPoints)
+{
 	std::vector<EVector3> points;
 
-	for (int i = 0; i < totalPoints; i++)
+	for (int i = 0; i < tPoints; i++)
 		points.push_back(EVector3((float)std::rand(), (float)std::rand(), (float)std::rand()));
 
-	EVector3 v{ 1.0f, 2.0f, 4.0f };
-	EVector3 u{ 5.0f, 6.0f, 7.0f };
+	std::vector<float> squareDistances;
 
-	EVector3 result = v - u;
-	float distance = result.magnitude();
-
-	std::cout << "Distance between: v" << v << " and u" << u << ": " << distance << std::endl;
-
-
-	/*std::vector<float> squareDistances;
-
-	for (int i = 0; i < totalPoints; i++)
+	for (int i = 0; i < tPoints; i++)
 	{
 		for (int j = 0; j < i; j++)
 		{
 			squareDistances.push_back((points[i] - points[j]).squareMagnitude());
 		}
 
-		for (int j = i + 1; j < totalPoints; j++)
+		for (int j = i + 1; j < tPoints; j++)
 		{
 			squareDistances.push_back((points[i] - points[j]).squareMagnitude());
 		}
@@ -48,12 +56,43 @@ int main()
 
 	std::sort(squareDistances.begin(), squareDistances.end());
 	for (int i = 0; i < 10; i++)
-		std::cout << std::sqrt(squareDistances[i]) << std::endl;*/
+		std::cout << std::sqrt(squareDistances[i]) << std::endl;
+}
 
-	const auto finish{ std::chrono::steady_clock::now() };
-	const std::chrono::duration<double> elapsedSeconds{ finish - start };
-	std::cout << "Elapsed Time: " << elapsedSeconds.count() << std::endl;
+void algorithmSortWithMag(int tPoints)
+{
+	std::vector<EVector3> points;
 
-	return 0;
+	for (int i = 0; i < tPoints; i++)
+		points.push_back(EVector3((float)std::rand(), (float)std::rand(), (float)std::rand()));
+
+	std::vector<float> distances;
+
+	for (int i = 0; i < tPoints; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			distances.push_back((points[i] - points[j]).squareMagnitude());
+		}
+
+		for (int j = i + 1; j < tPoints; j++)
+		{
+			distances.push_back((points[i] - points[j]).squareMagnitude());
+		}
+	}
+
+	std::sort(distances.begin(), distances.end());
+	for (int i = 0; i < 10; i++)
+		std::cout << distances[i] << std::endl;
+}
+
+void binaryTreeWithSquareMag(int tPoints)
+{
+
+}
+
+void binaryTreeWithMag(int tPoints)
+{
+
 }
 
